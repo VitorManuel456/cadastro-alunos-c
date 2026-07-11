f2e7423d41525cdff177cfc8ee9311cbee97574b
Vitor Manuel <165670280+VitorManuel456@users.noreply.github.com>
2026-07-10 21:14:09 -0300

    Implement student registration

--- src/main.c
+++ src/main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+#define MAX_ALUNOS 100
+
+typedef struct {
+    char nome[100];
+    int idade;
+    float nota;
+} Aluno;
+
+Aluno alunos[MAX_ALUNOS];
+int quantidade = 0;
+
 int main() {
 
     int opcao;
@@ -11,34 +22,70 @@ int main() {
         printf("=====================================\n");
         printf("1 - Cadastrar aluno\n");
         printf("2 - Listar alunos\n");
-        printf("3 - Buscar aluno\n");
         printf("0 - Sair\n");
-        printf("=====================================\n");
-        printf("Escolha uma opcao: ");
+        printf("Escolha: ");
         scanf("%d", &opcao);
 
-        switch(opcao) {
+        switch(opcao){
+
             case 1:
-                printf("\nCadastro ainda nao implementado.\n");
+
+                if(quantidade < MAX_ALUNOS){
+
+                    printf("Nome: ");
+                    scanf(" %[^\n]", alunos[quantidade].nome);
+
+                    printf("Idade: ");
+                    scanf("%d",&alunos[quantidade].idade);
+
+                    printf("Nota: ");
+                    scanf("%f",&alunos[quantidade].nota);
+
+                    quantidade++;
+
+                    printf("\nAluno cadastrado com sucesso!\n");
+
+                }else{
+
+                    printf("\nLimite de alunos atingido.\n");
+
+                }
+
                 break;
 
             case 2:
-                printf("\nListagem ainda nao implementada.\n");
-                break;
 
-            case 3:
-                printf("\nBusca ainda nao implementada.\n");
+                printf("\n===== LISTA DE ALUNOS =====\n");
+
+                if(quantidade == 0){
+
+                    printf("Nenhum aluno cadastrado.\n");
+
+                }else{
+
+                    for(int i=0;i<quantidade;i++){
+
+                        printf("\nAluno %d\n",i+1);
+                        printf("Nome : %s\n",alunos[i].nome);
+                        printf("Idade: %d\n",alunos[i].idade);
+                        printf("Nota : %.2f\n",alunos[i].nota);
+
+                    }
+
+                }
+
                 break;
 
             case 0:
-                printf("\nEncerrando o sistema...\n");
+                printf("\nSistema encerrado.\n");
                 break;
 
             default:
-                printf("\nOpcao invalida!\n");
+                printf("\nOpcao invalida.\n");
+
         }
 
-    } while(opcao != 0);
+    }while(opcao!=0);
 
     return 0;
 }
