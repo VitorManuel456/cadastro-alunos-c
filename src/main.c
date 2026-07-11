4806ede9498275e7cd94c6853de70fc78c447837
Vitor Manuel <165670280+VitorManuel456@users.noreply.github.com>
2026-07-10 21:10:59 -0300

    Create student registration structure

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("=====================================\n");
+    printf("     SISTEMA DE CADASTRO DE ALUNOS\n");
+    printf("=====================================\n");
+
+    return 0;
+}
