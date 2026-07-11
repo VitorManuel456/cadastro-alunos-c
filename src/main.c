7a2f338aada32d2f6870af2f1f90ecfc4183616c
Vitor Manuel <165670280+VitorManuel456@users.noreply.github.com>
2026-07-10 21:14:32 -0300

    Refactor code using functions

--- src/main.c
+++ src/main.c
@@ -2,82 +2,134 @@
 
 #define MAX_ALUNOS 100
 
-typedef struct {
+typedef struct{
+
     char nome[100];
     int idade;
     float nota;
-} Aluno;
+
+}Aluno;
 
 Aluno alunos[MAX_ALUNOS];
 int quantidade = 0;
 
-int main() {
+void mostrarMenu(){
+
+    printf("\n=====================================\n");
+    printf("      SISTEMA DE CADASTRO DE ALUNOS\n");
+    printf("=====================================\n");
+    printf("1 - Cadastrar aluno\n");
+    printf("2 - Listar alunos\n");
+    printf("3 - Buscar aluno\n");
+    printf("0 - Sair\n");
+    printf("Escolha: ");
+
+}
+
+void cadastrarAluno(){
+
+    if(quantidade >= MAX_ALUNOS){
+
+        printf("\nLimite de alunos atingido.\n");
+        return;
+
+    }
+
+    printf("Nome: ");
+    scanf(" %[^\n]", alunos[quantidade].nome);
+
+    printf("Idade: ");
+    scanf("%d",&alunos[quantidade].idade);
+
+    printf("Nota: ");
+    scanf("%f",&alunos[quantidade].nota);
+
+    quantidade++;
+
+    printf("\nAluno cadastrado com sucesso!\n");
+
+}
+
+void listarAlunos(){
+
+    if(quantidade==0){
+
+        printf("\nNenhum aluno cadastrado.\n");
+        return;
+
+    }
+
+    printf("\n===== ALUNOS CADASTRADOS =====\n");
+
+    for(int i=0;i<quantidade;i++){
+
+        printf("\nAluno %d\n",i+1);
+        printf("Nome : %s\n",alunos[i].nome);
+        printf("Idade: %d\n",alunos[i].idade);
+        printf("Nota : %.2f\n",alunos[i].nota);
+
+    }
+
+}
+
+void buscarAluno(){
+
+    char nomeBusca[100];
+    int encontrado = 0;
+
+    printf("Digite o nome: ");
+    scanf(" %[^\n]", nomeBusca);
+
+    for(int i=0;i<quantidade;i++){
+
+        if(strcmp(nomeBusca, alunos[i].nome)==0){
+
+            printf("\nAluno encontrado!\n");
+            printf("Nome : %s\n", alunos[i].nome);
+            printf("Idade: %d\n", alunos[i].idade);
+            printf("Nota : %.2f\n", alunos[i].nota);
+
+            encontrado = 1;
+            break;
+
+        }
+
+    }
+
+    if(!encontrado){
+
+        printf("\nAluno nao encontrado.\n");
+
+    }
+
+}
+
+int main(){
 
     int opcao;
 
-    do {
+    do{
 
-        printf("\n=====================================\n");
-        printf("      SISTEMA DE CADASTRO DE ALUNOS\n");
-        printf("=====================================\n");
-        printf("1 - Cadastrar aluno\n");
-        printf("2 - Listar alunos\n");
-        printf("0 - Sair\n");
-        printf("Escolha: ");
-        scanf("%d", &opcao);
+        mostrarMenu();
+
+        scanf("%d",&opcao);
 
         switch(opcao){
 
             case 1:
-
-                if(quantidade < MAX_ALUNOS){
-
-                    printf("Nome: ");
-                    scanf(" %[^\n]", alunos[quantidade].nome);
-
-                    printf("Idade: ");
-                    scanf("%d",&alunos[quantidade].idade);
-
-                    printf("Nota: ");
-                    scanf("%f",&alunos[quantidade].nota);
-
-                    quantidade++;
-
-                    printf("\nAluno cadastrado com sucesso!\n");
-
-                }else{
-
-                    printf("\nLimite de alunos atingido.\n");
-
-                }
-
+                cadastrarAluno();
                 break;
 
             case 2:
+                listarAlunos();
+                break;
 
-                printf("\n===== LISTA DE ALUNOS =====\n");
-
-                if(quantidade == 0){
-
-                    printf("Nenhum aluno cadastrado.\n");
-
-                }else{
-
-                    for(int i=0;i<quantidade;i++){
-
-                        printf("\nAluno %d\n",i+1);
-                        printf("Nome : %s\n",alunos[i].nome);
-                        printf("Idade: %d\n",alunos[i].idade);
-                        printf("Nota : %.2f\n",alunos[i].nota);
-
-                    }
-
-                }
-
+            case 3:
+                buscarAluno();
                 break;
 
             case 0:
-                printf("\nSistema encerrado.\n");
+                printf("\nEncerrando sistema...\n");
                 break;
 
             default:
@@ -88,4 +140,5 @@ int main() {
     }while(opcao!=0);
 
     return 0;
+
 }
